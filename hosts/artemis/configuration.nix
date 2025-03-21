17222befdc273eb1691c6ccf86fa6fa5fbb9ff06
Anonymous <anonymous@notgoogle.com>
2025-03-21 11:03:32 +0100

    Up

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -21,8 +21,48 @@
     username = "admin";
     password = "HMthisismys3cr3tP5ssword34a;";
   };
-  programs.suricata = { enable = true; };
+  services.suricata = { enable = true; settings = {
+
+    vars.address-groups.HOME_NET = "192.168.1.0/24";
+    pcap = [
+      {
+        interface = "ens19";
+      }
+    ];
+
+    outputs = [
+      {
+        fast = {
+          enabled = true;
+          filename = "fast.log";
+          append = "yes";
+        };
+      }
+      {
+        eve-log = {
+          enabled = true;
+          filetype = "regular";
+          filename = "eve.json";
+          community-id = true;
+          types = [
+            {
+              alert.tagged-packets = "yes";
+            }
+          ];
+        };
+      }
+    ];
+  }; };
   # headtails/tailscale
+  services.nginx = {
+    enable = true;
+    virtualHosts."localhost.local" = {
+    };
+  };
+  
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+
 
   # Don't touch this
   system.stateVersion = "24.05";
