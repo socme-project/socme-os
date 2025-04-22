9af0a5729a057b4e3089814aed7cfa5fc0d167d3
Anonymous <anonymous@notgoogle.com>
2025-04-22 11:47:19 +0200

    suricata debugged

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -21,39 +21,70 @@
     username = "admin";
     password = "HMthisismys3cr3tP5ssword34a;";
   };
-  services.suricata = { enable = true; settings = {
-
-    vars.address-groups.HOME_NET = "192.168.1.0/24";
-    pcap = [
-      {
+  services.suricata = {
+    enable = true;
+    settings = {
+      vars.address-groups.HOME_NET = "192.168.1.0/24";
+    
+      # Replace pcap with af-packet for better performance
+      af-packet = [{ 
         interface = "ens19";
-      }
-    ];
-
-    outputs = [
-      {
-        fast = {
+        cluster-id = 99;
+        cluster-type = "cluster_flow";
+        defrag = true;
+      }];
+    
+      # Fix the logging configuration structure
+      logging = {
+        default-log-level = "notice";
+        # Correct structure for logging outputs
+        console = {
           enabled = true;
-          filename = "fast.log";
-          append = "yes";
         };
-      }
-      {
-        eve-log = {
+        file = {
           enabled = true;
-          filetype = "regular";
-          filename = "eve.json";
-          community-id = true;
-          types = [
-            {
-              alert.tagged-packets = "yes";
-            }
-          ];
+          filename = "/var/log/suricata/suricata.log";
+          level = "info";
         };
-      }
-    ];
-  }; };
-  # headtails/tailscale
+      };
+      
+      # Configure outputs correctly
+      outputs = [
+        {
+          fast = {
+            enabled = true;
+            filename = "/var/log/suricata/fast.log";
+            append = "yes";
+          };
+        }
+        {
+          eve-log = {
+            enabled = true;
+            filetype = "regular";
+            filename = "/var/log/suricata/eve.json";
+            types = [
+              { alert = {}; }
+              { http = {}; }
+              { dns = {}; }
+              { tls = {}; }
+              { files = {}; }
+            ];
+          };
+        }
+      ];
+    
+      # Enable app-layer protocols including modbus
+      app-layer.protocols.modbus = {
+        enabled = "yes";
+        detection-enabled = "yes";
+      };
+    
+      # Make sure default ruleset is enabled
+      default-rule-path = "/var/lib/suricata/rules";
+      rule-files = [ "*.rules" ];
+    };
+  };
+     # headtails/tailscale
   services.nginx = {
     enable = true;
     virtualHosts."localhost.local" = {
