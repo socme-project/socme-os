b337a37b294f3bb20d272126d1e217d88b3c5dbd
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-04-22 09:41:24 +0200

    y

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -21,48 +21,42 @@
     username = "admin";
     password = "HMthisismys3cr3tP5ssword34a;";
   };
-  services.suricata = { enable = true; settings = {
 
-    vars.address-groups.HOME_NET = "192.168.1.0/24";
-    pcap = [
-      {
-        interface = "ens19";
-      }
-    ];
+  services.suricata = {
+    enable = true;
+    settings = {
 
-    outputs = [
-      {
-        fast = {
-          enabled = true;
-          filename = "fast.log";
-          append = "yes";
-        };
-      }
-      {
-        eve-log = {
-          enabled = true;
-          filetype = "regular";
-          filename = "eve.json";
-          community-id = true;
-          types = [
-            {
-              alert.tagged-packets = "yes";
-            }
-          ];
-        };
-      }
-    ];
-  }; };
+      vars.address-groups.HOME_NET = "192.168.1.0/24";
+      pcap = [{ interface = "ens19"; }];
+
+      outputs = [
+        {
+          fast = {
+            enabled = true;
+            filename = "fast.log";
+            append = "yes";
+          };
+        }
+        {
+          eve-log = {
+            enabled = true;
+            filetype = "regular";
+            filename = "eve.json";
+            community-id = true;
+            types = [{ alert.tagged-packets = "yes"; }];
+          };
+        }
+      ];
+    };
+  };
+
   # headtails/tailscale
   services.nginx = {
     enable = true;
-    virtualHosts."localhost.local" = {
-    };
+    virtualHosts."localhost.local" = { };
   };
-  
+
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-
-
 
   # Don't touch this
   system.stateVersion = "24.05";
