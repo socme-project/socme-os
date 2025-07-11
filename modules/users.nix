c9f1182f4fd32c712854763ef4066c0125debc2e
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-07-11 16:12:30 +0200

    uid
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- modules/users.nix
+++ modules/users.nix
@@ -5,6 +5,7 @@
     users.socme = {
       isNormalUser = true;
       home = "/home/socme";
+      uid = 1001;
       description = "SOCme account";
       extraGroups = [ "wheel" "networkmanager" "docker" ];
       initialHashedPassword = "$6$I3RukNU8KP0Djt4L$d1lMuR7n.tcDIvUKscNYVrHnGnzpy.t2JZ4kfJYDs0iwVWIlJRd8uVI8eCuRw8HA2pBZJq4.Qylj/0Di3Wge11";
