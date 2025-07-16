ff7d3f7f1df0ec98b9922626c38a84305d3242b8
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-07-16 10:35:34 +0200

    change key
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- modules/users.nix
+++ modules/users.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.zsh.enable = true;
   users = {
     defaultUserShell = pkgs.zsh;
@@ -7,13 +7,13 @@
       home = "/home/socme";
       uid = 1001;
       description = "SOCme account";
-      extraGroups = [ "wheel" "networkmanager" "docker" ];
+      extraGroups = ["wheel" "networkmanager" "docker"];
       initialHashedPassword = "$6$I3RukNU8KP0Djt4L$d1lMuR7n.tcDIvUKscNYVrHnGnzpy.t2JZ4kfJYDs0iwVWIlJRd8uVI8eCuRw8HA2pBZJq4.Qylj/0Di3Wge11";
 
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGBTOOWuJQZc9qTNswozTYhje4eYFv2OOmuprZgnSDJ hadi@nixy"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGBTOOWuJQZc9qTNswozTYhje4eYFv2OOmuprZgnSDJ ekip"
       ];
     };
   };
-  nix.settings.allowed-users = [ "socme" ];
+  nix.settings.allowed-users = ["socme"];
 }
